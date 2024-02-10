c4b273ab4e85f0bedd28f027e73691f5764086c4
ccu3 <98501033+ciaraugasmoy@users.noreply.github.com>
2024-02-10 15:58:20 -0500

    Update loginRequest.php less hardcoding

--- loginRequest.php
+++ loginRequest.php
@@ -3,17 +3,19 @@ require_once('path.inc');
 require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
 
-
-$client = new rabbitMQClient("testRabbitMQ.ini","testServer");
+$client = new rabbitMQClient("testRabbitMQ.ini", "testServer");
 $msg = "test message";
 
 $request = array();
 $request['type'] = "Login";
-$request['username'] = "mr loginrequest";
-$request['password'] = "password";
+$request['username'] = $_POST['username'];  // Retrieve the username from the POST request
+$request['password'] = $_POST['password'];  // Retrieve the password from the POST request
 $request['message'] = $msg;
-//$response = $client->send_request($request);
-$response = $client->publish($request);
+
+// still unsure the big diff btwn send_request vs publish
+$response = $client->send_request($request);
+//$response = $client->publish($request);
 
 $payload = json_encode($response);
 echo $payload;
+?>
