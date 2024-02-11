5cf64aa997cbc9bf250ea74608d2335eeb42467f
ccu3 <98501033+ciaraugasmoy@users.noreply.github.com>
2024-02-10 20:17:50 -0500

    Update testRabbitMQServer.php

--- testRabbitMQServer.php
+++ testRabbitMQServer.php
@@ -4,37 +4,53 @@ require_once('path.inc');
 require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
 
-function doLogin($username,$password)
+function doLogin($username, $password)
 {
-    // lookup username in databas
-    // check password
-    return true;
-    //return false if not valid
+    // Connect to MySQL database
+    $mysqli = new mysqli("localhost", "testuser", "12345", "testdb");
+
+    // Check connection
+    if ($mysqli->connect_error) {
+        die("Connection failed: " . $mysqli->connect_error);
+    }
+
+    // Sanitize input to prevent SQL injection
+    $username = $mysqli->real_escape_string($username);
+    $password = $mysqli->real_escape_string($password);
+
+    // Perform login check
+    $query = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
+    $result = $mysqli->query($query);
+
+    if ($result->num_rows > 0) {
+        $mysqli->close();
+        return true;
+    } else {
+        $mysqli->close();
+        return false;
+    }
 }
 
 function requestProcessor($request)
 {
-  echo "received request".PHP_EOL;
-  var_dump($request);
-  if(!isset($request['type']))
-  {
-    return "ERROR: unsupported message type";
-  }
-  switch ($request['type'])
-  {
-    case "login":
-      return doLogin($request['username'],$request['password']);
-    case "validate_session":
-      return doValidate($request['sessionId']);
-  }
-  return array("returnCode" => '0', 'message'=>"Server received request and processed");
+    echo "received request" . PHP_EOL;
+    var_dump($request);
+    if (!isset($request['type'])) {
+        return "ERROR: unsupported message type";
+    }
+    switch ($request['type']) {
+        case "login":
+            return doLogin($request['username'], $request['password']);
+        case "validate_session":
+            return doValidate($request['sessionId']);
+    }
+    return array("returnCode" => '0', 'message' => "Server received request and processed");
 }
 
-$server = new rabbitMQServer("testRabbitMQ.ini","testServer");
+$server = new rabbitMQServer("testRabbitMQ.ini", "testServer");
 
-echo "testRabbitMQServer BEGIN".PHP_EOL;
+echo "testRabbitMQServer BEGIN" . PHP_EOL;
 $server->process_requests('requestProcessor');
-echo "testRabbitMQServer END".PHP_EOL;
+echo "testRabbitMQServer END" . PHP_EOL;
 exit();
 ?>
-
