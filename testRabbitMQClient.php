60abff7519f29ca32a5210755e7a1507c1869c86
ciara <ccu3@njit.edu>
2024-02-14 21:54:41 -0500

    rename exchange

--- testRabbitMQClient.php
+++ testRabbitMQClient.php
@@ -17,14 +17,14 @@ else
 $request = array();
 $request['type'] = "login";
 $request['username'] = "steve";
-$request['password'] = "password";
+$request['password'] = "12345";
 $request['message'] = $msg;
 echo "request sent".PHP_EOL;
 $response = $client->send_request($request);
 //$response = $client->publish($request);
 
 echo "client received response: ".PHP_EOL;
-//print_r($response);
+print_r($response);
 echo "\n\n";
 
 echo $argv[0]." END".PHP_EOL;
