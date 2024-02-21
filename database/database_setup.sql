e853db54f6103432e253efcf6e3526af03a96ee9
ccu3 <98501033+ciaraugasmoy@users.noreply.github.com>
2024-02-21 15:33:15 -0500

    Update database_setup.sql

--- database/database_setup.sql
+++ database/database_setup.sql
@@ -4,18 +4,15 @@ CREATE DATABASE IF NOT EXISTS what2watch;
 -- Switch to the created database
 USE what2watch;
 
--- Create the 'users' table
 CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(255) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL
+    username VARCHAR(255) UNIQUE NOT NULL
 );
--- table for public keys to validate users
-CREATE TABLE IF NOT EXISTS public_keys (
+
+CREATE TABLE IF NOT EXISTS private_keys (
     id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT NOT NULL,
-    public_key TEXT NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    private_key VARCHAR(255) NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
