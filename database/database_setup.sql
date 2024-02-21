1e8ef07bc8fa0c41f4523772eb8e1fe6d57a9413
ccu3 <98501033+ciaraugasmoy@users.noreply.github.com>
2024-02-21 11:37:15 -0500

    added table to hold public keys

--- database/database_setup.sql
+++ database/database_setup.sql
@@ -10,9 +10,17 @@ CREATE TABLE IF NOT EXISTS users (
     username VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL
 );
+-- table for public keys to validate users
+CREATE TABLE IF NOT EXISTS public_keys (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    public_key TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
 
 -- Create a user and grant limited permissions
-CREATE USER 'what2watchadmin'@'localhost' IDENTIFIED BY 'what2watchpassword';
+CREATE USER IF NOT EXISTS 'what2watchadmin'@'localhost' IDENTIFIED BY 'what2watchpassword';
 GRANT SELECT, INSERT, UPDATE, DELETE ON what2watch.users TO 'what2watchadmin'@'localhost';
 -- Add additional GRANT statements for other tables if needed
 FLUSH PRIVILEGES;
