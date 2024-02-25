067133033b64b320e1fe6681db72d24c01a59723
ccu3 <98501033+ciaraugasmoy@users.noreply.github.com>
2024-02-25 17:26:23 -0500

    Update database_setup.sql

--- database/database_setup.sql
+++ database/database_setup.sql
@@ -4,10 +4,16 @@ CREATE DATABASE IF NOT EXISTS what2watch;
 -- Switch to the created database
 USE what2watch;
 
+-- Drop tables if they exist so we can overwrite future changes completely
+DROP TABLE IF EXISTS private_keys;
+DROP TABLE IF EXISTS users;
+
 CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(255) UNIQUE NOT NULL,
-    password VARCHAR(64) NOT NULL
+    password VARCHAR(64) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    dob DATE NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS private_keys (
@@ -17,8 +23,7 @@ CREATE TABLE IF NOT EXISTS private_keys (
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
--- Create a user and grant limited permissions
+-- user with grant limited permissions
 CREATE USER IF NOT EXISTS 'what2watchadmin'@'localhost' IDENTIFIED BY 'what2watchpassword';
 GRANT SELECT, INSERT, UPDATE, DELETE ON what2watch.* TO 'what2watchadmin'@'localhost';
--- Add additional GRANT statements for other tables if needed
 FLUSH PRIVILEGES;
