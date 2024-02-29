cf8b1f5f5d30529cf388cc3370c48efa6243577f
Reggie Erciyas <rse9@njit.edu>
2024-02-29 18:12:14 -0500

    Create client_rpc.php

---
+++ client_rpc.php
@@ -0,0 +1,78 @@
+#!/usr/bin/php
+
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+
+class RPCClient
+{
+    private $connection;
+    private $channel;
+    private $callback_queue;
+    private $response;
+    private $corr_id;
+
+    public function __construct()
+    {
+        $this->connection = new AMQPStreamConnection(
+            'localhost',
+            5672,
+            'testUser',
+            'testpassword'
+        );
+        $this->channel = $this->connection->channel();
+        list($this->callback_queue, ,) = $this->channel->queue_declare(
+            "",
+            false,
+            false,
+            true,
+            false
+        );
+        $this->channel->basic_consume(
+            $this->callback_queue,
+            '',
+            false,
+            true,
+            false,
+            false,
+            array(
+                $this,
+                'onResponse'
+            )
+        );
+    }
+
+    public function onResponse($rep)
+    {
+        if ($rep->get('correlation_id') == $this->corr_id) {
+            $this->response = $rep->body;
+        }
+    }
+
+    public function call($n)
+    {
+        $this->response = null;
+        $this->corr_id = uniqid();
+
+        $msg = new AMQPMessage(
+            (string) $n,
+            array(
+                'correlation_id' => $this->corr_id,
+                'reply_to' => $this->callback_queue
+            )
+        );
+        $this->channel->basic_publish($msg, '', 'testQueue');
+        while (!$this->response) {
+            $this->channel->wait();
+        }
+        return intval($this->response);
+    }
+}
+
+$client_rpc = new RPCClient();
+$response = $client_rpc->call(); #ENTER WHAT U WANT (MESSAGE) INSIDE OF CALLss
+echo ' [.] Got ', $response, "\n";
+
+?>
