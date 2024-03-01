30ae5b58a138100839e5ad1596150d92dc4ee1b3
ciaraugasmoy <ccu3@njit.edu>
2024-03-01 13:47:33 -0500

    db connections

--- database/database_setup.sql
+++ database/database_setup.sql
@@ -24,6 +24,10 @@ CREATE TABLE IF NOT EXISTS private_keys (
 );
 
 -- user with grant limited permissions
+
 CREATE USER IF NOT EXISTS 'what2watchadmin'@'localhost' IDENTIFIED BY 'what2watchpassword';
 GRANT SELECT, INSERT, UPDATE, DELETE ON what2watch.* TO 'what2watchadmin'@'localhost';
+FLUSH PRIVILEGES;
+CREATE USER IF NOT EXISTS 'vdbadmin'@'10.147.20.78' IDENTIFIED BY 'what2watchpassword';
+GRANT SELECT, INSERT, UPDATE, DELETE ON what2watch.* TO 'vdbadmin'@'10.147.20.78';
 FLUSH PRIVILEGES;
---
+++ database/readme.md
@@ -0,0 +1,4 @@
+#binding addresses, subject to change
+'''
+echo -e "[mysqld]\nbind-address = 0.0.0.0" | sudo tee -a /etc/mysql/my.cnf
+'''
