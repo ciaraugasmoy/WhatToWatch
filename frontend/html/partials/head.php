8a56712b0d7236f84738643c2dd0a17fc5b5e88d
ciaraugasmoy <ccu3@njit.edu>
2024-03-02 21:53:04 -0500

    ui things

---
+++ frontend/html/partials/head.php
@@ -0,0 +1,6 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Your Website Title</title>
+    <link rel="stylesheet" href="<?= __DIR__ ?>/css/global.css">
+</head>
---
+++ frontend/html/partials/header.php
@@ -0,0 +1,6 @@
+<header id="header">
+<img src="/logo.png"><img>
+<h1>
+    What2Watch
+</h1>
+</header>
