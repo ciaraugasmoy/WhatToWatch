a903393ac8996f2df2935d0a3c47aebb1d1670a2
ciaraugasmoy <ccu3@njit.edu>
2024-03-08 00:52:48 -0500

    tinyf

--- frontend/html/pages/home.php
+++ frontend/html/pages/home.php
@@ -30,14 +30,28 @@ $response = $client->call($request);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> <?php echo $username; ?> Home</title>
+    <style scoped>
+        .providers{
+            display: flex;
+        }
+        .providers img{
+            max-width: 30px;
+            border-radius: 5px;
+        }
+    </style>
 </head>
 <body>
 
     <h2>Home</h2>
+    <section>
+    <h3>WatchList</h3>
+    <div class="providers">
     <?php
         foreach ($response['watch_provider_info'] as $key => $value) {
-            echo " <img src= 'https://image.tmdb.org/t/p/w500/" . $value['logo_path']. "'><h3>". $value['provider_name'] . "</h3>";
+            echo "<div class='provider'> <img src= 'https://image.tmdb.org/t/p/w500/" . $value['logo_path']. "'>" . "</p></div>";
         }
     ?>
+    </div>
+    </section>
 </body>
 </html>
