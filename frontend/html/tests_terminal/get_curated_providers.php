e1eefe4921b35f9a9e1add3049587065f17bd249
ciaraugasmoy <ccu3@njit.edu>
2024-03-09 13:29:08 -0500

    test for get curated_providers.php

---
+++ frontend/html/tests_terminal/get_curated_providers.php
@@ -0,0 +1,17 @@
+#!/usr/bin/php
+
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+require_once '../client/client_rpc.php'; // Include the RPCClient class
+
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+
+$client_rpc = new RPCClient();
+
+$request = array();
+$request['type'] = "get_curated_providers";
+$response = $client_rpc->call($request);
+echo ' [.] Response: '.PHP_EOL;
+var_dump($response);
