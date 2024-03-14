2a8cbb05393fe525439e86dda4f8e85134861557
ciaraugasmoy <ccu3@njit.edu>
2024-03-14 14:55:36 -0400

    pagination front end

--- frontend/html/tests_terminal/discover_movie.php
+++ frontend/html/tests_terminal/discover_movie.php
@@ -2,16 +2,12 @@
 <?php
 
 require_once __DIR__ . '/../vendor/autoload.php';
-require_once '../client/client_rpc.php'; // Include the RPCClient class
+require_once __DIR__ . '../client/client_rpc.php'; // Include the RPCClient class
 
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 use PhpAmqpLib\Message\AMQPMessage;
 
 $client = new RPCClient();
-
-$username='ciara';
-$watchproviderid=7;
-
 
 $request=array();
 $request['type'] = "discover_movie";
