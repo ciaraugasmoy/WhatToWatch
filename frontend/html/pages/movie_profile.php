82eceb1eb4523cb6f1583154ac3392d99a9bed1d
ciaraugasmoy <ccu3@njit.edu>
2024-03-15 15:14:25 -0400

    movie_profile css

--- frontend/html/pages/movie_profile.php
+++ frontend/html/pages/movie_profile.php
@@ -9,6 +9,30 @@
     <title>movie page</title>
 </head>
 <body>
+<style scoped>
+
+.backdrop{
+    position: fixed;
+    z-index: -1;
+    width: 100vw;   
+}
+.movieinfo{
+    margin:100px 10px 20px;
+    border-radius: 20px;
+    background: linear-gradient(#000000 5px,#000000DD);
+    max-width:500px;
+    padding: 20px;
+    justify-self: center;
+    justify-content: center;
+    justify-items: center;
+    gap:10px;
+    display: grid;
+}
+.movieinfo .poster{
+    width: 300px;
+    border-radius: 20px;
+}
+</style>
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../client/client_rpc.php';
@@ -30,10 +54,10 @@ $movie = $response['movie'];
     $backdrop_url= 'https://image.tmdb.org/t/p/original/'.$movie['backdrop_path'];
     $poster_url= 'https://image.tmdb.org/t/p/original/'.$movie['poster_path'];
     echo
-    '<section class="movieinfo">'
+    '<img class="backdrop" src="'.$backdrop_url.'">'
+    .'<section class="movieinfo">'
     .'<h2>'.$movie['title'].'</h2>'
-    .'<p>'.$movie['release_date'].'</p>'
-    .'<img class="backdrop" src="'.$backdrop_url.'">'
+    .'<p class="releasedate">'.$movie['release_date'].'</p>'
     .'<p>'.$movie['overview'].'</p>'
     .'<img class="poster" src="'.$poster_url.'">'
     .'<p>'.'remember the wp'.'</p>'
