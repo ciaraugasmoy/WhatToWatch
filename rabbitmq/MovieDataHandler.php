926a3a9f14698db34db56c69ef92e9bc601537a1
gbj3 <gbj3@njit.edu>
2024-03-15 01:51:12 -0400

    db created and posting/getting started

--- rabbitmq/MovieDataHandler.php
+++ rabbitmq/MovieDataHandler.php
@@ -86,8 +86,61 @@ class MovieDataHandler
             }
         }
     }
-    
-}
 
+	public function getForumDiscussion($username, $movie_id, $message_id, $parent_message_id)
+	{
+		$username = $this->mysqli->real_escape_string($username);
+		$movie_id = $this->mysqli->real_escape_string($movie_id);
+		$message_id = $this->mysqli->real_escape_string($message_id);
+		$parent_message_id = $this->mysqli->real_escape_string($parent_message_id);
+		
+		$userQuery = "SELECT id FROM users WHERE username = '$username'";
+		$userResult = $this->mysqli->query($userQuery);
+		$userData = $userResult->fetch_assoc();
+		$user_id = $userData['id'];
+
+		$query = "SELECT * FROM discussion_posts WHERE user_id = $user_id AND movie_id = $movie_id AND message_id = $message_id";
+		$result = $this->mysqli->query($query);
+
+		if(!$result) {
+			return ['status' => 'error', 'message' => $this->mysqli->error];
+		}
+		if($result->num_rows ==0) {
+			return ['status' => 'success', 'message' => 'No discussion messages by user'];
+		}
+		$reviewData = $result->fetch_assoc();
+		return ['status' => 'success', 'user_discussion_data' => $reviewData];
+	}
+
+	public function postForumDiscussion($username, $movie_id, $message_id, $text, $parent_message_id)
+	{
+		$username = $this->mysqli->real_escape_string($username);
+		$movie_id = $this->mysqli->real_escape_string($movie_id);
+ 		$message_id = $this->mysqli->real_escape_string($message_id);
+		$text = $this->mysqli->real_escape_string($text);
+		$parent_message_id = $this->mysqli->real_escape_string($parent_message_id);
+		
+		$userQuery = "SELECT id FROM users WHERE username = '$username'";
+		$userResult = $this->mysqli->query($userQuery);
+      $userData = $userResult->fetch_assoc();
+      $user_id = $userData['id'];
+
+		try {
+			$movieQuery = "SELECT id FROM movies WHERE movie_id = $movie_id";
+			$movieResult = $this->mysqli->query($movieQuery);
+
+		} catch (mysqli_sql_exception $exception) {
+			return ['status' => 'error', 'message' => 'Failed to retrieve movie data: ' . $exception->getMessage()];
+		}
+		$insertQuery = "INSERT INTO discussion_posts (message_id, movie_id, user_id, parent_message_id, text) VALUES ($message_id, $movie_id, $user_id, $parent_message_id, $text)";
+		
+		try {
+			$insertResult = $this->mysqli->query($insertQuery);
+			return ['status' => 'success', 'message' => 'Discussion message posted'];	
+		} catch {
+			return ['status' => 'error', 'message' => 'Failed to post message: ' . $exception->getMessage()];
+		}
+	}
+}
 
 ?>
