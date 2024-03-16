14cc0e58c79d09517baa21055afc7b6367ed2e8a
ciaraugasmoy <ccu3@njit.edu>
2024-03-16 18:15:42 -0400

    removed json header type

--- frontend/html/pages/profile.php
+++ frontend/html/pages/profile.php
@@ -1,3 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/global.css">
+    <script src="../javascript/template.js"></script>
+    <title>movie page</title>
+</head>
+<body>
+<style scoped>
+</style>
+
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../client/client_rpc.php';
@@ -7,8 +20,6 @@ use PhpAmqpLib\Message\AMQPMessage;
 
 $client = new RPCClient();
 
-header('Content-Type: application/json');
-$username2 = $_POST['friend_username'];
 $access_token = isset($_COOKIE['access_token']) ? $_COOKIE['access_token'] : '';
 $refresh_token = isset($_COOKIE['refresh_token']) ? $_COOKIE['refresh_token'] : '';
 $username = isset($_COOKIE['username']) ? $_COOKIE['username'] : '';
@@ -34,5 +45,23 @@ if (empty($access_token) || empty($refresh_token) || empty($username)) {
             'friend_name' => $friend_name,
         ];
         $response = $client->call($request);
-        echo json_encode($response);
+        if ($response['status'] === 'success') {
+            $reviews = $response['reviews'];
+        
+            foreach ($reviews as $review) {
+                echo "Review ID: " . $review['id'];
+                echo "Movie ID: " . $review['movie_id'];
+                echo "User ID: " . $review['user_id'] ;
+                echo "Rating: " . $review['rating'] ;
+                echo "Review: " . $review['review'] ;
+                echo "Created At: " . $review['created_at'] ;
+                echo "Movie Title: " . $review['movie_title'] ;
+                echo "Movie Poster Path: " . $review['movie_poster_path'];
+            }
+        } else {
+            echo "Error: " . $response['message'];
+        }
 ?>
+
+</body>
+</html>
