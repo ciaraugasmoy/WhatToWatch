5477d93940445a8ccf49f76b68904b26b62ccb1f
ciaraugasmoy <ccu3@njit.edu>
2024-03-16 18:51:43 -0400

    error msg in div not friends yt

--- frontend/html/pages/profile.php
+++ frontend/html/pages/profile.php
@@ -9,8 +9,40 @@
 </head>
 <body>
 <style scoped>
-</style>
+#review-box{
+justify-self: center;
+}
+.review{
+    margin:10px 0px 20px;
+    border-radius: 20px;
+    background: linear-gradient(#001E38 5px,#001E38DD);
+    width:500px;
+    max-width: 100vw;
+    padding: 20px;
+    justify-self: center;
+    justify-content: center;
+    justify-items: center;
+    gap:10px;
+    display: grid;
+}
+.poster{
+    width: 300px;
+    border-radius: 20px;
+}
+.poster img{
+    width:100%;
+}
+h2:first-of-type{
+    text-align: center;
+    margin-top:20px;
+}
 
+</style>
+<?php 
+$usr=$_GET['username'];
+echo '<h2>'."Welcome to $usr's profile!".'</h2>';
+?>
+<section id="review-box">
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../client/client_rpc.php';
@@ -36,7 +68,6 @@ if (empty($access_token) || empty($refresh_token) || empty($username)) {
         ],
         'username' => $username,
     ];
-    echo "Welcome to $username's profile!";
 
     $response = $client->call($request);
         $request = [
@@ -49,19 +80,30 @@ if (empty($access_token) || empty($refresh_token) || empty($username)) {
             $reviews = $response['reviews'];
         
             foreach ($reviews as $review) {
-                echo "Review ID: " . $review['id'];
-                echo "Movie ID: " . $review['movie_id'];
-                echo "User ID: " . $review['user_id'] ;
-                echo "Rating: " . $review['rating'] ;
-                echo "Review: " . $review['review'] ;
-                echo "Created At: " . $review['created_at'] ;
-                echo "Movie Title: " . $review['movie_title'] ;
-                echo "Movie Poster Path: " . $review['movie_poster_path'];
+                // echo "Review ID: " . $review['id'];
+                // echo "Movie ID: " . $review['movie_id'];
+                // echo "User ID: " . $review['user_id'] ;
+
+                $url= 'https://image.tmdb.org/t/p/original'.$review['movie_poster_path'];
+                $rating=$review['rating'];
+                    $stars='';
+                    for ($x = 0; $x <= $rating; $x++) {
+                        $stars .= '★';
+                    }
+                echo '<div class="review">'
+                .'<div class="content">'
+                .'<h3>'.'Title:'.$review['movie_title'].'</h3>'
+                .'<p>'.$review['review'].'<span>'.$review['created_at'].'</span>'.'</p>'
+                .'<div class="stars">'.$stars.'</div>'
+                .'</div>'
+                .'<div class="poster">'.'<img src="'.$url.'">'.'</div>'
+                .'</div>';
+
             }
         } else {
-            echo "Error: " . $response['message'];
+            echo '<div class="review">'. $response['message'].'</div>';
         }
 ?>
-
+</section>
 </body>
 </html>
