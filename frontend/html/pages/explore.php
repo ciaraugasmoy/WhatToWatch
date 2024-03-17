bc0554c6709432a871248764a9dd0bff14bd66a4
ciaraugasmoy <ccu3@njit.edu>
2024-03-16 21:12:35 -0400

    min vi forum

--- frontend/html/pages/explore.php
+++ frontend/html/pages/explore.php
@@ -9,10 +9,34 @@
     <script src="../javascript/globalscript.js"></script>
 </head>
 <body>
-<main style='margin: 10px 0;'>
-    <h2>Home</h2>
-    <?php include '../partials/settings_streaming.php'; ?>
-    <?php include '../partials/friends.php'; ?>
-</main>
+<h2>Explore</h2>
+<h3>Latest Discussions</h3>
+<?php
+require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../client/client_rpc.php'; // Include the RPCClient class
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+$client = new RPCClient();
+
+$offset=0;
+$limit=5;
+
+$request = array();
+$request['type'] = "get_recent_threads";
+$request['offset'] = $offset;
+$request['limit'] = $limit;
+$request['query'] = '';
+$response = $client->call($request);
+if ($response['status'] === 'success') {
+    foreach ($response['threads'] as $thread){
+        echo '<div class="thread">'
+        .'<h4>'.$thread['title'].'</h4>'
+        .'<p>'.$thread['body'].'</p>'
+        .'<button value="'.$thread['id'].'">'.'See More'.'</button>'
+        ;
+    }
+}
+
+?>
 </body>
 </html>
