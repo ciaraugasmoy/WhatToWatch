0f56b5abb81b2a8e2656fe1e256892f3488681a2
gbj3 <vboxuser@490test.myguest.virtualbox.org>
2024-05-01 21:06:26 -0400

    more efficient

--- logging/logScript.sh
+++ logging/logScript.sh
@@ -6,7 +6,7 @@ while true
 do
     php logHandler.php
 
-    sshpass -p "$password" rsync -avz -e ssh ./logs/WhatToWatch.log vboxuser@172.27.129.66:~/WhatToWatch/logging/logs/WhatToWatch.log
+    sshpass -p "$password" rsync -avz -e ssh ./logs/what2watch.log vboxuser@172.27.129.66:~/WhatToWatch/logging/logs/what2watch.log
 
     sleep 10
 done
